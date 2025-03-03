b7ee460a25b95a296395b207105eb66918d5515b
Vikarn Jha <vikarnjha91@gmail.com>
2025-03-04 04:00:20 +0530

    added the functionality of Sign In || Sign Up button

--- react-authxpert/src/components/Signup.jsx
+++ react-authxpert/src/components/Signup.jsx
@@ -1,39 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Signup.css";
 import { FaUser, FaEnvelope, FaLock } from "react-icons/fa";
 
-
 const Signup = () => {
+  const [action, setAction] = useState("Sign Up");
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>{" "}
       <div className="inputs">
+        {action === "Sign Up" && (
+          <div className="input">
+            <FaUser className="icon" />
+            <input type="text" placeholder="Username" />
+          </div>
+        )}
         <div className="input">
-        <FaUser className="icon" />
-          <input type="text" placeholder="Name" />
-        </div>
-        <div className="input">
-        <FaEnvelope className="icon" />
+          <FaEnvelope className="icon" />
           <input type="email" placeholder="Email" />
         </div>
         <div className="input">
-        <FaLock className="icon" />
+          <FaLock className="icon" />
           <input type="password" placeholder="Password" />
         </div>
-        <div className="input">
-        <FaLock className="icon" />
-          <input type="password" placeholder="Confirm Password" />
+        {action === "Sign Up" && (
+          <div className="input">
+            <FaLock className="icon" />
+            <input type="password" placeholder="Confirm Password" />
+          </div>
+        )}
+      </div>
+      {action === "Sign In" && (
+        <div className="forgot-password">
+          Forget Password? <span>Click Here!</span>
         </div>
-      </div>
-      <div className="forgot-password">
-        Forget Password? <span>Click Here!</span>
-      </div>
+      )}
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit red">Sign In</div>
+        <div
+          className={action === "Sign Up" ? "submit red" : "submit"}
+          onClick={() => setAction("Sign Up")}
+        >
+          Sign Up
+        </div>
+        <div
+          className={action === "Sign In" ? "submit red" : "submit"}
+          onClick={() => setAction("Sign In")}
+        >
+          Sign In
+        </div>
       </div>
     </div>
   );
