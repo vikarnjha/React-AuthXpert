41091d7264d03bf6d6c0dec1fe36dbcb29b23ad2
Vikarn Jha <vikarnjha91@gmail.com>
2025-03-04 03:16:38 +0530

    signup page structure defined

--- react-authxpert/src/components/Signup.jsx
+++ react-authxpert/src/components/Signup.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="container">
+      <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>{" "}
+      <div className="inputs">
+        <div className="input">
+          <img src="" alt="" />
+          <input type="text" placeholder="Name" />
+        </div>
+        <div className="input">
+          <img src="" alt="" />
+          <input type="email" placeholder="Email" />
+        </div>
+        <div className="input">
+          <img src="" alt="" />
+          <input type="password" placeholder="Password" />
+        </div>
+      </div>
+      <div className="forgot-password">
+        Forget Password? <span>Click Here!</span>
+      </div>
+      <div className="submit-container">
+        <div className="submit">Sign Up</div>
+        <div className="submit gray">Sign In</div>
+      </div>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
